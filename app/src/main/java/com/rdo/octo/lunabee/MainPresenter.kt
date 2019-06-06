5b47a76a3e787a92923a5e5fedb1a48c183d8bb3
Remi Dormoy <rdormoy@octo.com>
2019-06-06 12:02:35 +0200

    added search

--- app/src/main/java/com/rdo/octo/lunabee/MainPresenter.kt
+++ app/src/main/java/com/rdo/octo/lunabee/MainPresenter.kt
@@ -5,18 +5,27 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainPresenter(private val view: MainView, private val service: UserService) :
-    Callback<List<User>> {
+class MainPresenter(private val view: MainView, private val service: UserService) : Callback<List<User>> {
+
+    private var list = emptyList<User>()
+
     override fun onFailure(call: Call<List<User>>, t: Throwable) {
         Log.e("YOLO TAG", "Error calling service", t)
         view.displayError()
     }
 
     override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
-        view.displayUsers(response.body() ?: emptyList())
+        list = response.body() ?: emptyList()
+        view.displayUsers(list)
     }
 
     fun getArticles() {
         service.getUsers().enqueue(this)
+    }
+
+    fun search(query: String){
+        view.displayUsers(list.filter {
+            it.first_name.toLowerCase().contains(query.toLowerCase())
+        })
     }
 }
